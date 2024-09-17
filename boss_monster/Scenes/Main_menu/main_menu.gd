cb26f1925c7fd649537f8cb06329acfaadda1d2e
MrNaimi <antti.niemi4@hotmail.com>
2024-09-17 15:47:05 +0300

    Play button added that changes scene

---
+++ boss_monster/Scenes/Main_menu/main_menu.gd
@@ -0,0 +1,19 @@
+extends Control
+
+
+var Dungeon_Scene = preload("res://Scenes/Dungeon/Dungeon.tscn")
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+
+func _on_play_button_pressed() -> void:
+	print("Play button was pressed")
+	get_tree().change_scene_to_packed(Dungeon_Scene)
