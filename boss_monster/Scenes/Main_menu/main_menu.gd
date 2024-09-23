f8c34abb43bb64c42ee73c9af6f32ee2aba1f7e1
Iiro <iiro.paakkonen@gmail.com>
2024-09-23 20:33:55 +0300

    Muutettu fonttia, lisätty collections-menu ja nappeja

--- boss_monster/Scenes/Main_menu/main_menu.gd
+++ boss_monster/Scenes/Main_menu/main_menu.gd
@@ -22,3 +22,6 @@ func _on_play_button_pressed() -> void:
 
 func _on_quit_game_pressed() -> void:
 	get_tree().quit()
+
+func _on_collections_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Main_menu/collections.tscn")
