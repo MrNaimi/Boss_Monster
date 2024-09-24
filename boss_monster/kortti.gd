9dc3a318b4079f53bb215d6cc2d7a1f5ba4f4663
MrNaimi <antti.niemi4@hotmail.com>
2024-09-24 15:15:40 +0300

    HEHEHE

--- boss_monster/kortti.gd
+++ boss_monster/kortti.gd
@@ -1,27 +1,50 @@
 extends Node2D
 
+var draggable = false
+var is_inside_droppable = false
+var body_ref
+var offset: Vector2
+var initialPos
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
-func _on_area_2d_mouse_entered() -> void:
 	pass
 	
+func _process(delta: float) -> void:
+	if draggable:
+		
+		if Input.is_action_just_pressed("click"):
+			var initialPos = global_position
+			offset = get_global_mouse_position() - global_position
+			Dungeon.is_dragging = true
+			
+		if Input.is_action_pressed("click"):
+			global_position = get_global_mouse_position()
+		elif Input.is_action_just_released("click"):
+			Dungeon.is_dragging
+			var tween = get_tree().create_tween()
+			if is_inside_droppable:
+				tween.tween_property(self, "position", body_ref.position,0.2).set_ease(Tween.EASE_OUT)
+			else:
+				tween.tween_property(self, "global_position", initialPos,0.2).set_ease(Tween.EASE_OUT)
+				
+func _on_area_2d_mouse_entered() -> void:
+	if not Dungeon.is_dragging:
+		draggable = true
+		scale = Vector2(1.05, 1.05)
 
 func _on_area_2d_mouse_exited() -> void:
-	pass # Replace with function body.
-
+	if not Dungeon.is_dragging:
+		draggable = false
+		scale = Vector2(1, 1)
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
-
+	if body.is_in_group('droppable'):
+		is_inside_droppable = true
+		body.modulate = Color(Color.AZURE)
+		body_ref = body
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
-	pass # Replace with function body.
+	if body.is_in_group('droppable'):
+		is_inside_droppable = false
+		body.modulate =  Color(Color.BEIGE)
