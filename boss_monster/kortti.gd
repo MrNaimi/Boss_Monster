466b294a236b07e407ea464688f479874e12e78f
MrNaimi <antti.niemi4@hotmail.com>
2024-09-27 20:38:30 +0300

    Säädin kortti scriptiä

--- boss_monster/kortti.gd
+++ boss_monster/kortti.gd
@@ -6,16 +6,18 @@ var body_ref = null
 var offset: Vector2
 var initialPos
 var heh = Vector2(60.00, -10.00)
+var card_frame: Node2D
 
 func _ready() -> void:
 	initialPos = global_position  # Store the initial position correctly
+	card_frame = find_child("Card_frame")
 	
 func _process(delta: float) -> void:
 	if draggable:
 		if Input.is_action_just_pressed("click"):
 			offset = get_global_mouse_position() - global_position
 			Dungeon.is_dragging = true
-			scale = Vector2(2.00, 2.00)
+			card_frame.scale = Vector2(0.1, 0.1)
 			
 		if Input.is_action_pressed("click"):
 			global_position = get_global_mouse_position()
@@ -32,7 +34,7 @@ func _on_area_2d_mouse_entered() -> void:
 	if not Dungeon.is_dragging:
 		var tween = get_tree().create_tween()
 		draggable = true
-		scale = Vector2(4.00, 4.00)
+		card_frame.scale = Vector2(0.2, 0.2)
 		position = global_position + Vector2(0,-100)
 		
 		
@@ -41,8 +43,13 @@ func _on_area_2d_mouse_exited() -> void:
 	print("pois päältä")
 	if not Dungeon.is_dragging:
 		draggable = false
-		scale = Vector2(2.00, 2.00)
-		position = global_position + Vector2(0,100)
+		card_frame.scale = Vector2(0.1, 0.1)
+		
+		#Snaps card to position when it touches dragspot, otherwise goes to initialposition
+		if body_ref != null:
+			position = body_ref.global_position - heh
+		else:
+			position = initialPos
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group('droppable'):
