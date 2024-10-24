51d2379d209c954c163768887d17d4e07b7a85a7
Vuori1 <78846315+Vuori1@users.noreply.github.com>
2024-10-24 22:46:28 +0300

    database update

--- boss_monster/Database/Database.gd
+++ boss_monster/Database/Database.gd
@@ -4,9 +4,11 @@
 #SpellInfo =[Type, Damage, +Health, Name, Special Text]
 #RoomInfo =[Type, Damage, Name, Monster or Trap, Special Text]
 
-enum {Cleric,Paladin, Monk, Barbarian, Mage, Ranger, Lucky_Fellow, Skeleton_King, Lich, Big_Snake, Shrink_Ray, Corrupted_Mage, Mind_Control, Healing_potion, Assassination, Bad_Directions, Goblin_warrior, Gas_Leak, Mimic, The_Vault_Room, Spike_Factory, Hot_Coals, Monster_Lounge,}
+enum {Cleric,Paladin, Monk, Barbarian, Mage, Ranger, Lucky_Fellow, Skeleton_King, Lich, Big_Snake, Shrink_Ray, Corrupted_Mage, Mind_Control, Healing_potion, Assassination, Bad_Directions, Goblin_warrior, Gas_Leak, Mimic, The_Vault_Room, Spike_Factory, Hot_Coals, Monster_Lounge,The_Dragon_Lair, Pit_Fall, Spike_Trap,}
 
 const DATA ={
+	
+	#Hero
 	Cleric :
 		["Hero", 2, 4 ,"Cleric", "Healer", "Holy_Item", ""],
 	Paladin :
@@ -21,6 +23,9 @@ const DATA ={
 		["Hero", 4, 5, "Ranger", "Damage", "Weapon_item", ""],
 	Lucky_Fellow :
 		["Hero", 1, 2, "Lucky_Fellow", "Damage", "all", ""],
+		
+		
+	#Boss
 	Skeleton_King :
 		["Boss", 20, "Skeleton_King", "Wants to raise an army of smaller and smaller skeletons"],
 	Lich :
@@ -29,6 +34,8 @@ const DATA ={
 		["Boss"],
 	Corrupted_Mage: 
 		["Boss", 20, "Corrupted_Mage", "Will not rest until he has the strongest magic!"],
+		
+	#Spells
 	Shrink_Ray :
 		["Spell",0,0, "Shrink Ray", "Reduce heros maximum HP by 5"],
 	Mind_Control :
@@ -39,6 +46,8 @@ const DATA ={
 		["Spell", 100, 0, "Assassination", "Kill a targeted hero"],
 	Bad_Directions :
 		["Spell", 0, 0, "Bad Directions", "Return a hero back to the start of the dungeon"],
+		
+	#Rooms
 	Goblin_warrior :
 		["Room", 3, "Goblin Warrior", "Monster", ""],
 	Gas_Leak :
@@ -48,9 +57,17 @@ const DATA ={
 	The_Vault_Room :
 		["Room", 3, "The Vault Room","Monster", ""],
 	Spike_Factory :
-		["Room",4, "Spike Factory","Trap",""],
+		["Room", 4, "Spike Factory","Trap",""],
 	Hot_Coals:
-		["Room",3, "Hot Coals","Trap",""],	
+		["Room", 3, "Hot Coals","Trap",""],
 	Monster_Lounge:
-		["Room",4, "Monster Lounge", "Monster", ""],	
+		["Room", 4, "Monster Lounge", "Monster", ""],
+	The_Dragon_Lair: 
+		["Room", 4, "The Dragon Lair", "Monster", ""],
+	Pit_Fall:
+		["Room", 1, "Pit Fall", "Trap", "Kill the hero in this room"],
+	Spike_Trap :
+		["Room", 3, "Spike Trap", "Trap", "Deal 6 damage to the hero in this room"],
 } 
+#Kortit joille on art tehty:
+#Healing Potion, Gas Leak, Assassination
