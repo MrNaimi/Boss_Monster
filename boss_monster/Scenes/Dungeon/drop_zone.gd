4ca7271a9e640c812172e09b4addd855f4052ef7
tuulosss <samu@tuulos.net>
2024-10-30 10:38:10 +0200

    hover animaatio toimii

--- boss_monster/Scenes/Dungeon/drop_zone.gd
+++ boss_monster/Scenes/Dungeon/drop_zone.gd
@@ -1,6 +1,7 @@
 extends ColorRect
 
 @onready var drop_zone: ColorRect = $"."
+@onready var animation_player: AnimationPlayer = $"../AnimationPlayer"
 
 var i=0
 # Called when the node enters the scene tree for the first time.
@@ -12,9 +13,10 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if GlobalVariables.card_dragging:
 		self.visible=true
-		
+		animation_player.play("hover")
 	else:
 		self.visible=false
+		animation_player.stop()
 	#print(self.get_parent().get_child(0).get_child_count())
 	if self.get_parent().get_child(0).get_child_count():
 		self.visible=false
