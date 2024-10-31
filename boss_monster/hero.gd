61a69ddd77dfd59ac55643a2bc5eb2e67bd15d0e
MrNaimi <antti.niemi4@hotmail.com>
2024-10-31 15:51:11 +0200

    heiwh

--- boss_monster/hero.gd
+++ boss_monster/hero.gd
@@ -1,8 +1,9 @@
 extends Node2D
+@onready var health_bar: ProgressBar = $healthBar
 
 @onready var idle_animation: AnimatedSprite2D = $Idle_animation
 @onready var timer: Timer = $Timer
-var hp = 10
+var hp = GlobalVariables.heroHp
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
@@ -16,8 +17,13 @@ func _process(delta: float) -> void:
 		idle_animation.flip_h = false
 		
 	timer.wait_time = GlobalVariables.timerAmount
-	
+	health_bar.value = hp
 	print(hp)
+	
+	if hp <= 0:
+		queue_free()
+		GlobalVariables.heroKilled = true
+		
 	
 
 func _on_hit_box_area_entered(area: Area2D) -> void:
@@ -32,3 +38,4 @@ func _on_timer_timeout() -> void:
 
 func _on_hit_box_area_exited(area: Area2D) -> void:
 	hp -= GlobalVariables.damageGiven
+	GlobalVariables.heroHp = hp
