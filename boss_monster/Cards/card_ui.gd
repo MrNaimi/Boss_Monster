cdf05096ba8de68aeea709b21e5b99ccec50822e
MrNaimi <antti.niemi4@hotmail.com>
2024-11-01 14:27:09 +0200

    Revert "Reapply "Merge branch 'main' of https://github.com/MrNaimi/Boss_Monster""
    This reverts commit a3e88504728a48afb11ceb2d685644a4c5a7690f.

--- boss_monster/Cards/card_ui.gd
+++ boss_monster/Cards/card_ui.gd
@@ -71,5 +71,5 @@ func _on_hit_box_area_entered(area: Area2D) -> void:
 
 
 func _on_hit_box_area_exited(area: Area2D) -> void:
-	GlobalVariables.spawn_hero=true
+	#GlobalVariables.spawn_hero=true
 	pass
