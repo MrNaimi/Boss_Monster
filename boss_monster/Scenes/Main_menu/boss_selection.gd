61a5ea96a15a91de9315d2aa182f72671ff96cec
Henri Vuori <henri.vuori01@gmail.com>
2024-12-10 12:49:57 +0200

    boss selection done

--- boss_monster/Scenes/Main_menu/boss_selection.gd
+++ boss_monster/Scenes/Main_menu/boss_selection.gd
@@ -12,4 +12,10 @@ func _process(delta):
 
 
 func _on_button_pressed():
+	$Demon.visibility_layer = 0
+	$Button.visibility_layer = 0
+	$Lore.visibility_layer = 1
+	$Timer.start(2)
+
+func _on_timer_timeout():
 	Transition.change_scene("res://Scenes/Dungeon/Dungeon.tscn")
