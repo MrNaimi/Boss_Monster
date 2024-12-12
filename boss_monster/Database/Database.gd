1e7fbbdc003d9dbea2618388e905fbc16527a1e1
Henri Vuori <henri.vuori01@gmail.com>
2024-12-12 14:20:47 +0200

    databasea korjattu ja lisätty 10 korttia

--- boss_monster/Database/Database.gd
+++ boss_monster/Database/Database.gd
@@ -3,8 +3,71 @@
 #BossInfo =[Type, Health, Name, Special Text]
 #SpellInfo =[Type, Damage, +Health, Name, Special Text, png]
 #RoomInfo =[Type, Damage, Name, Monster or Trap, Special Text, png, sound, Tribe]
-
-enum {Cleric,Paladin, Monk, Barbarian, Mage, Ranger, Lucky_Fellow, Skeleton_King, Lich, Big_Snake, Shrink_Ray, Corrupted_Mage, Mind_Control, Healing_potion, Assassination, Bad_Directions, Goblin_warrior, Gas_Leak, Mimic, The_Vault_Room, Spike_Factory, Hot_Coals, Monster_Lounge,The_Dragon_Lair, Pit_Fall, Spike_Trap, Forgotten_Library, Succubus, Vampire, Misunderstood_Ghost, Zombie_Graveyard, Stinky_Ghoul, Rolling_Golem, Killer_Robot, Angry_Slime, Fire_Elemental, Imp, Warlock_Summoner, Demon_Spawn, Lesser_Devil, Outlaw,Orc_Bodyguard,Goblin_Army, Pack_of_Wolves, Chihu, Lions_Den, Electric_Anomaly}
+enum {
+	# Heroes
+	Cleric,
+	Paladin,
+	Monk,
+	Barbarian,
+	Mage,
+	Ranger,
+	Lucky_Fellow,
+	
+	# Bosses
+	Skeleton_King,
+	Lich,
+	Big_Snake,
+	Corrupted_Mage,
+	
+	# Spells
+	Shrink_Ray,
+	Mind_Control,
+	Healing_Potion,
+	Assassination,
+	Bad_Directions,
+	
+	# Rooms
+	Goblin_Warrior,
+	Gas_Leak,
+	Mimic,
+	The_Vault_Room,
+	Spike_Factory,
+	Hot_Coals,
+	Monster_Lounge,
+	The_Dragon_Lair,
+	Pit_Fall,
+	Spike_Trap,
+	Forgotten_Library,
+	Succubus,
+	Vampire,
+	Stinky_Ghoul,
+	Misunderstood_Ghost,
+	Zombie_Graveyard,
+	Rolling_Golem,
+	Killer_Robot,
+	Angry_Slime,
+	Fire_Elemental,
+	Imp,
+	Warlock_Summoner,
+	Demon_Spawn,
+	Lesser_Devil,
+	Outlaw,
+	Orc_Bodyguard,
+	Goblin_Army,
+	Pack_of_Wolves,
+	Chihu,
+	Lions_Den,
+	Electric_Anomaly,
+	Gator,
+	Last_Mammoth,
+	Repair_Bot,
+	Amalgamation,
+	Skeleton_Ceo,
+	Ominous_Shadow,
+	Summoning_Circle,
+	Fallen_Angle
+}
+#enum {Cleric,Paladin, Monk, Barbarian, Mage, Ranger, Lucky_Fellow, Skeleton_King, Lich, Big_Snake, Shrink_Ray, Corrupted_Mage, Mind_Control, Healing_potion, Assassination, Bad_Directions, Goblin_warrior, Gas_Leak, Mimic, The_Vault_Room, Spike_Factory, Hot_Coals, Monster_Lounge,The_Dragon_Lair, Pit_Fall, Spike_Trap, Forgotten_Library, Succubus, Vampire, Misunderstood_Ghost, Zombie_Graveyard, Stinky_Ghoul, Rolling_Golem, Killer_Robot, Angry_Slime, Fire_Elemental, Imp, Warlock_Summoner, Demon_Spawn, Lesser_Devil, Outlaw,Orc_Bodyguard,Goblin_Army, Pack_of_Wolves, Chihu, Lions_Den, Electric_Anomaly,Gator,Last_Mammoth, Repair_Bot, Amalgamation, Ominous_Shadow, Summoning_Circle, Fallen_Angle, Greedy_Goblin,Goblin_General,Skeleton_Ceo}
 
 const DATA ={
 	#HeroInfo =[Type, Factor, Hp lower limit, Hp upper limit , Name, Role, Special Text]
@@ -37,10 +100,10 @@ const DATA ={
 
 	#Spells
 	Shrink_Ray :
-		["Spell","50%", 0, "Shrink Ray", "Reduce heros maximum HP by 5","shrink_ray.png"],
+		["Spell","50%", 0, "Shrink Ray", "Reduce heros maximum HP by 50%","shrink_ray.png"],
 	Mind_Control :
-		["Spell",100 , 0, "Mind Control", "Targeted Hero deals damage to an adjacent hero","mind_control.png"],
-	Healing_potion :
+		["Spell",100 , 0, "Mind Control", "Targeted Hero becomes mindcontrolled and will attack its friends. Effect breaks on damage.","mind_control.png"],
+	Healing_Potion :
 		["Spell","+10hp", 10, "Healing Potion", "Restore 10 health to your Boss","healing_potion.png"],
 	Assassination :
 		["Spell", 100, 0, "Assassination", "Kill a targeted hero","assassination.png"],
@@ -49,20 +112,20 @@ const DATA ={
 		
 	#Rooms 
 	#RoomInfo =[Type, Damage, Name, Monster or Trap, Special Text, png, sound, Tribe]
-	Goblin_warrior : #tehty oikee art
+	Goblin_Warrior : #tehty oikee art
 		["Room", 3, "Goblin Warrior", "Monster", "The Goblin Warrior has a 1 in 4 chance to get extra +2 gold from killed Heroes (does not stack with itself)", "goblin_warrior.png","goblin_warrior.wav","humanoid"],
 	Gas_Leak : #tehty oikee art
 		["Room", 2, "Gas Leak", "Trap", "Activate this trap to deal 6 damage to every hero in the dungeon", "gas_leak.png","","trap"],
 	Mimic :#tehty oikee art
 		["Room", 3, "Mimic", "Trap", "Activate this trap to deal damage to a hero equal to your gold", "mimic.png","mimic.wav","trap"],
 	The_Vault_Room : #tehty 
-		["Room", 3, "The Vault Room","Trap", "Activate this trap to consume 5 gold and kill a hero in this room (not currently implemented)", "vault_room.png","vault.wav","trap"],
+		["Room", 3, "The Vault Room","Trap", "Activate this trap to consume 5 gold and kill a hero in this room.", "vault_room.png","vault.wav","trap"],
 	Spike_Factory :#tehty
 		["Room", 4, "Spike Factory","Monster","Your “Spike Traps” deal +3dmg", "spike_factory.png","", "construct"],
 	Hot_Coals: #tehty oikee art
 		["Room", 4, "Hot Coals","Trap","Activate this trap to burn the hero, dealing 3 damage for every room they enter", "hot_coals.png","","trap"],
 	Monster_Lounge: #tehty
-		["Room", 3, "Skeleton Lounge", "Monster", "Destroying this room gives +2 undead damage", "monster_lounge.png","","undead"],
+		["Room", 3, "Skeleton Lounge", "Monster", "Destroying this room gives +2 dmg to your undead rooms", "monster_lounge.png","","undead"],
 	The_Dragon_Lair: #tehty
 		["Room", 5, "Dragon Lair", "Monster", "if you kill a hero in this room, get 2 gold", "dragon_lair.png","","beast"],
 	Pit_Fall: #tehty oikee art
@@ -90,15 +153,15 @@ const DATA ={
 	Fire_Elemental:
 		["Room", 2, "Fire Elemental", "Monster", "Hero takes 1 damage for every room they entered","fire_elemental.png","","construct"],
 	Imp:
-		["Room", 2, "Demonic Scout", "Monster", "When a hero passes this room they take +3 dmg from “Demon” type Rooms","imp.png","","demon"],
+		["Room", 2, "Demonic Scout", "Monster", "When a hero passes this room give your demon rooms +1dmg","imp.png","","demon"],
 	Warlock_Summoner:
-		["Room", 3, "Warlock Summoner", "Monster", "Your 'Demon' type rooms deal +2dmg","warlock_summoner.png","","demon, Humanoid"],
+		["Room", 3, "Warlock Summoner", "Monster", "For each unique demon you control, gain +4dmg","warlock_summoner.png","","demon, Humanoid"],
 	Demon_Spawn:
 		["Room", 3, "Demon Spawn", "Monster", "has +3dmg if you have a “Lesser devil” in your dungeon","demon_spawn.png","","demon"],
 	Lesser_Devil:
 		["Room", 4, "Lesser Devil", "Monster", "Gains +0.5 dmg for each “Demon” type room placed","lesser_devil.png","","demon"],
 	Outlaw:
-		["Room", 7, "Outlaw", "Monster", "He takes 2 gold every turn as a wage, if you have less than 2 gold, If you can't afford him, he leaves","outlaw.png","","humanoid"],
+		["Room", 7, "Outlaw", "Monster", "Takes 2 gold every turn as a wage, if you have less than 2 gold, he leaves","outlaw.png","","humanoid"],
 	Orc_Bodyguard:
 		["Room", 4, "Orc Bodyguard", "Monster", "Gives +2dmg to Humanoids but takes 3 gold per turn as a wage. If you can't afford him, he leaves","orc_bodyguard.png","","humanoid"],
 	Goblin_Army:
@@ -111,4 +174,21 @@ const DATA ={
 		["Room", 3, "Lions Den", "Monster", "Gives your beast rooms +1dmg for each beast in your dungeon","lions_den.png","","beast"],
 	Electric_Anomaly:
 		["Room", 3, "Electric Anomaly", "Monster", "gives +2 dmg to your “Construct” and “Trap” Rooms","electric_anomaly.png","","construct"],
+	Gator:
+		["Room",6, "Gator","Monster","If this kills a hero, enrage your other beasts for 3 rounds","Gator.png","","beast"],
+	Last_Mammoth:
+		["Room", 10, "The Last Mammoth", "Monster", "Deals double damage if there is no room before it","last_mammoth.png", "", "beast"],
+	Repair_Bot:
+		["Room", 2, "Repair Bot", "Monster", "Reduces the chance of self destruction by 10%", "repair_bot.png", "", "construct"],
+	Amalgamation:
+		["Room", 5, "Amalgamation", "Monster", "This has all Tribes", "amalgamation.png", "", "construct"],
+	Skeleton_Ceo:
+		["Room", 1, "Skeleton CEO", "Monster", "When this kills a hero, add a Skeleton Lounge to your hand", "skeleton_ceo.png", "", "undead"],
+	Ominous_Shadow:
+		["Room", 5, "Ominous Shadow", "Monster", "if you control a stinky ghoul and misunderstood ghost, give +3 to your undead rooms", "ominous_shadow.png", "", "undead"],
+	Summoning_Circle:
+		["Room", 3, "Summoning Circle", "Monster", "has +2 for each Succubus you control and gives +2dmg to each of your Demonic Scouts", "pentagram.png", "", "demon"],
+	Fallen_Angle:
+		["Room", 5, "Fallen Angle", "Monster", "Gains 3 dmg for each successful succubus charm","fallen_angel.png", "", "demon"]
+	
 } 
